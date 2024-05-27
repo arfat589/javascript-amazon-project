cdd806b800f5b1e07ebafc5e79d1ce216bfa3bb8
hussainArfat <arhussain4321@gmail.com>
2024-05-27 15:57:04 +0545

    Make payment summary interractive

--- Script/checkout/orderSummary.js
+++ Script/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js'
-hello();
-
-const today = dayjs();
-
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd ,MMM D'))
+import {renderPaymentSummary} from './paymentSummary.js'
 
 export function renderOrderSummary() {
 
@@ -127,6 +122,8 @@ export function renderOrderSummary() {
             `.js-cart-item-container-${productId}`)
             
             container.remove();
+
+            renderPaymentSummary()
       });
       });
 
@@ -137,6 +134,7 @@ export function renderOrderSummary() {
          const deliveryOptionId =element.dataset.deliveryOptionId;
          updateDeliveryOption(productId,deliveryOptionId)
          renderOrderSummary();
+         renderPaymentSummary();
          
       })
       })
