47539ae369360733c50c7e8709546736eb6043d1
hussainArfat <arhussain4321@gmail.com>
2024-06-24 14:28:33 +0545

    Learn error handeling

--- data/products.js
+++ data/products.js
@@ -145,10 +145,13 @@ export let products = [];
     })
     console.log('load products')
 
+  }).catch((error) => {
+    console.log('unexpected error.Please try again later.')
   });
 
   return promise;
 }
+
 
 /*
 loadProductsFetch().then(() => {
@@ -169,10 +172,17 @@ export function loadProducts(fun) {
     console.log('load products')
 fun();
   });
+
+  xhr.addEventListener('error',(error) => {
+    console.log('unexpected error.Please try again later.')
+  });
+
+ 
   
   xhr.open('GET','https://supersimplebackend.dev/products')
   xhr.send()
 };
+
 
 /*
  export const products = [
